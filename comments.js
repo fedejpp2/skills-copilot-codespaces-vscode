8013e6fb5e6992adfb14d4bfbfe04275ecb0afb2
fedejpp2 <152441063+fedejpp2@users.noreply.github.com>
2023-11-29 20:48:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,60 @@
-// create web server
+// Create web server 
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
-const comment = require('./comment');
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
 
-const app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+const server = http.createServer((req, res) => {
 
-app.get('/comments', (req, res) => {
-    res.send(
-        comment.comments
-    )
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('Hello');
+    });
+
+    greet();
+    greet();
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // routing
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // send html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
 });
 
-app.post('/comments', (req, res) => {
-    const newComment = req.body;
-    console.log(newComment);
-    comment.comments.push(newComment);
-    res.send({
-        success: true,
-        message: 'Comment added successfully!'
-    })
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000');
 });
-
-app.listen(process.env.PORT || 8081);
